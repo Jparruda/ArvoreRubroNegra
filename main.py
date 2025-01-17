6de4685332b2623a8769cd746b5b8c62ebbe9a88
João Pedro Souza Arruda <125840345+Jparruda@users.noreply.github.com>
2025-01-17 12:08:47 +0000

    testes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import arvore_rubro_negra
 tree = arvore_rubro_negra.ArvoreRubroNegra()
 
 # Inserir elementos na árvore
-elements = [20, 18, 17, 15, 25, 10, 5, 3, 2, 1]
+elements = [30,20,10]
 for elem in elements:
     tree.insert(elem)
 
@@ -10,7 +10,7 @@ print("Árvore após inserções:")
 print(tree)
 
 # Buscar um elemento na árvore
-key_to_search = 15
+key_to_search = 20
 search_result = tree.search(key_to_search)
 if search_result.key is not None:
     print(f"\nElemento {key_to_search} encontrado na árvore.")
@@ -18,15 +18,21 @@ else:
     print(f"\nElemento {key_to_search} não encontrado na árvore.")
 
 # Deletar um elemento da árvore
-key_to_delete = 15
-tree.delete(key_to_delete)
-print(f"\nÁrvore após deletar o elemento {key_to_delete}:")
+tree.delete(20)
+#  print(f"\nÁrvore após deletar o elemento {20}:")
 print(tree)
 
-novo_valor = 22
-print(f"\nInserindo o valor {novo_valor} na árvore...")
-tree.insert(novo_valor)
 
-# Imprime a árvore após a nova inserção
-print("\nÁrvore após inserir o novo valor:")
-print(tree)
+# print(f"\nInserindo o valor 22 na árvore...")
+# tree.insert(22)
+
+# # Imprime a árvore após a nova inserção
+# print("\nÁrvore após inserir o novo valor:")
+# print(tree)
+
+# print(f"\deletando o valor 18 na árvore...")
+# tree.delete(18)
+
+# # Imprime a árvore após a nova inserção
+# print("\nÁrvore após deletar valor:")
+# print(tree)
